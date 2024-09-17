df86913d3e107d0e2849840025ab686d7dc4d282
YefriRonaldinhoLermaPacompia <ronallerma033@gmail.com>
2024-09-17 00:24:40 -0700

    exam

--- src/main/java/pe/edu/upeu/tictactoe/Application.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upeu.tictactoe;
-
-import pe.edu.upeu.tictactoe.control.TictacControl;
-
-public class Application {
-    public static void main(String[] args) {
-        TcTacToeApplication.main(args);
-    }
-}
